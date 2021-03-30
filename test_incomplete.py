87d60a7782a44e2dd7851f61d5d69074fead30fe
David Johnston <dajohnst@thoughtworks.com>
2021-03-30 12:03:49 -0400

    using unittest

--- test_incomplete.py
+++ test_incomplete.py
@@ -1,37 +1,32 @@
 from university_incomplete import count_countries, sort_by_chosen_score
+import unittest
 
 
-def test_count_countires():
-    data = [{'country': 'US', 'foo': 42}, {'country': 'US', 'bar': 99}, {'country': 'Canada'}]
-    expected = [('US', 2), ('Canada', 1)]
+class TestCountCountries(unittest.TestCase):
+    def test_count_countires(self):
+        data = [{'country': 'US', 'foo': 42}, {'country': 'US', 'bar': 99}, {'country': 'Canada'}]
+        expected = [('US', 2), ('Canada', 1)]
 
-    result = count_countries(data)
-    assert expected == result
+        result = count_countries(data)
+        self.assertListEqual(expected, result)
 
 
-def test_sort_by_chosen_score():
-    data = [{'foo': 42, 'bar': '99'},
-            {'foo': 0, 'bar': '888'},
-            {'foo': 0, 'bar': 0},
-            {'foo': 0, 'bar': '-5'}]
-
-    sorted_list = sort_by_chosen_score(data, 'bar')
-
-    expected = [{'foo': 0, 'bar': '888'},
-                {'foo': 42, 'bar': '99'},
+class TestSortByChosenScore(unittest.TestCase):
+    def test_sort_by_chosen_score(self):
+        data = [{'foo': 42, 'bar': '99'},
+                {'foo': 0, 'bar': '888'},
                 {'foo': 0, 'bar': 0},
                 {'foo': 0, 'bar': '-5'}]
 
-    for i, kv in enumerate(sorted_list):
-        expected_kv = expected[i]
-        if kv != expected_kv:
-            print('Error - Items are not equal')
-            print(kv, expected_kv)
-            assert kv == expected_kv
+        sorted_list = sort_by_chosen_score(data, 'bar')
+
+        expected = [{'foo': 0, 'bar': '888'},
+                    {'foo': 42, 'bar': '99'},
+                    {'foo': 0, 'bar': 0},
+                    {'foo': 0, 'bar': '-5'}]
+
+        self.assertListEqual(sorted_list, expected)
 
 
 if __name__ == "__main__":
-    # run in python to avoid pytest dependency
-    test_count_countires()
-    test_sort_by_chosen_score()
-    print('Tests pass')
+    unittest.main()
